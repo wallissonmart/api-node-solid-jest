7f1dc0fc4bd528c27ab263f4ff8f9f248ba2c884
Wallisson Martins <wallissonmartins37@gmail.com>
2023-06-15 09:20:35 -0300

    modificação server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+  res.send('Hello, world!!!');
 });
 
 app.listen(port, () => {
