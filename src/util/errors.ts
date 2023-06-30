1f3e678abbd35a1c3c29968477b3bfa48261ae7a
Wallisson Martins <wallissonmartins37@gmail.com>
2023-06-29 23:31:06 -0300

    config errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -2,6 +2,7 @@ const customMessage = (field: string) => {
   return {
     'string.empty': `O campo ${field} não pode ser vazio!`,
     'any.required': `O campo ${field} é obrigatório!`,
+    'string.base': `O campo ${field} precisa ser uma string!`,
   };
 };
 
